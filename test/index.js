cf972cccf3292745a1eeb9c30662eb8cb1120eb1
tormozz48 <tormozz48@gmail.com>
2015-06-01 01:06:05 +0300

    Add src task modules moved from bs-builder-core. Add test for github module. Add tests scaffolds for other modules

---
+++ test/index.js
@@ -0,0 +1,3 @@
+require('../lib/tasks/docs-load-gh');
+require('../lib/tasks/docs-load-file');
+require('../lib/tasks/docs-md-html');
---
+++ test/tasks/docs-load-file.test.js
@@ -0,0 +1,12 @@
+var os = require('os'),
+    fs = require('fs'),
+    mockFs = require('mock-fs'),
+    should = require('should'),
+    Config = require('bs-builder-core/lib/config'),
+    Model = require('bs-builder-core/lib/model/model'),
+    DocsLoadFile = require('../../lib/tasks/docs-load-file');
+
+describe('DocsLoadFile', function () {
+
+});
+
