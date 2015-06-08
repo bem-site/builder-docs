0a9fa1c3da700cde8b6bc9a5fcea14c4ae75cd2d
tormozz48 <tormozz48@gmail.com>
2015-06-08 17:55:18 +0300

    Add index.js file for suitable plugging

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.tasks = {
+    DocsGhLoad: require('./lib/tasks/docs-gh-load'),
+    DocsFileLoad: require('./lib/tasks/docs-file-load'),
+    DocsMdHtml: require('./lib/tasks/docs-md-html')
+};
--- test/index.js
+++ test/index.js
@@ -3,3 +3,17 @@ require('./tasks/docs-gh-base.test.js');
 require('./tasks/docs-gh-load.test.js');
 require('./tasks/docs-file-load.test.js');
 require('./tasks/docs-md-html.test');
+
+var index = require('../index');
+
+it('should plug DocsGhLoad task', function () {
+    index.tasks.DocsGhLoad.getName().should.equal('docs load from gh');
+});
+
+it('should plug DocsFileLoad task', function () {
+    index.tasks.DocsFileLoad.getName().should.equal('docs load from file');
+});
+
+it('should plug DocsMdHtml task', function () {
+    index.tasks.DocsMdHtml.getName().should.equal('docs markdown to html');
+});
